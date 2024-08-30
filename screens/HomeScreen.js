2487e4fc161921ac4a3e0f9e8a2be9bcf290f8f0
Patthanan Vachirathatthira <Patthananmint.v@gmail.com>
2024-08-31 01:50:55 +0700

    add search bar

---
+++ screens/HomeScreen.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { View, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity
+        style={styles.searchBarContainer}
+        onPress={() => navigation.navigate('RouteSearchScreen')}
+        activeOpacity={1} // ให้สามารถกดได้ทั้งกล่อง
+      >
+        <TextInput
+          style={styles.searchBar}
+          placeholder="คุณจะไปไหนดี"
+          placeholderTextColor="#888"
+          editable={false} // ป้องกันการแก้ไขข้อความใน TextInput
+        />
+        <Ionicons
+          name="md-search"
+          size={24}
+          color="#888"
+          style={styles.searchIcon}
+          onPress={() => navigation.navigate('RouteSearchScreen')}
+        />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    backgroundColor: '#f8f8f8',
+  },
+  searchBarContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    padding: 6,
+    width: '80%',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.10,
+    shadowRadius: 1,
+    elevation: 1.5,
+  },
+  searchBar: {
+    flex: 1,
+    height: 40,
+    borderRadius: 30,
+    paddingLeft: 10,
+  },
+  searchIcon: {
+    marginLeft: 10,
+  },
+});
+
+export default HomeScreen
