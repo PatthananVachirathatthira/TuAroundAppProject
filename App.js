2ea59630fb70eda79f12274c5a4fba73ad7ae5ce
Patthanan Vachirathatthira <Patthananmint.v@gmail.com>
2024-08-30 14:17:48 +0700

    hot fix

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Tu Around</Text>
+      <Text>Application</Text>
       <StatusBar style="auto" />
     </View>
   );
