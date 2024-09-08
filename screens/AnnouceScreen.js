5986d067b88287ee25f9b6ef5914bce5b85ddcdf
Patthanan Vachirathatthira <Patthananmint.v@gmail.com>
2024-09-08 23:56:03 +0700

    change

--- screens/AnnouceScreen.js
+++ screens/AnnouceScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet, Dimensions } from 'react-native';
+import { View, Text, StyleSheet, Dimensions, ScrollView } from 'react-native';
 import { AntDesign } from '@expo/vector-icons';
 
 const { width } = Dimensions.get('window'); // ใช้ขนาดของหน้าจอ
@@ -14,7 +14,6 @@ const AnnounceScreen = ({ route }) => {
           <View style={styles.contentContainer}>
             <Text style={styles.header}>การปรัปปรุง EV สาย A</Text>
             <Text style={styles.date}>32 สิงหา 2024-5.00</Text>
-            {/* เพิ่มเนื้อหาเพิ่มเติมที่นี่ */}
           </View>
         );
       case 'B':
@@ -22,7 +21,6 @@ const AnnounceScreen = ({ route }) => {
           <View style={styles.contentContainer}>
             <Text style={styles.header}>การปรัปปรุง EV สาย B</Text>
             <Text style={styles.date}>32 สิงหา 2024-5.00</Text>
-            {/* เพิ่มเนื้อหาเพิ่มเติมที่นี่ */}
           </View>
         );
       case 'C':
@@ -30,7 +28,37 @@ const AnnounceScreen = ({ route }) => {
           <View style={styles.contentContainer}>
             <Text style={styles.header}>การปรัปปรุง EV สาย C</Text>
             <Text style={styles.date}>32 สิงหา 2024-5.00</Text>
-            {/* เพิ่มเนื้อหาเพิ่มเติมที่นี่ */}
+          </View>
+        );
+      default:
+        return null;
+    }
+  };
+
+  const renderAdditionalInfo = () => {
+    switch (type) {
+      case 'A':
+        return (
+          <View style={styles.additionalInfoContainer}>
+            <Text style={styles.additionalInfoText}>
+            Dolor voluptate aliquip et duis exercitation quis laborum ad. Dolore nulla exercitation nulla sunt pariatur consequat reprehenderit labore proident nisi fugiat non. Adipisicing aute sunt dolore irure Lorem elit esse voluptate amet fugiat.
+            </Text>
+          </View>
+        );
+      case 'B':
+        return (
+          <View style={styles.additionalInfoContainer}>
+            <Text style={styles.additionalInfoText}>
+            Nostrud in est occaecat occaecat cupidatat ex ex. Cupidatat officia consequat amet cupidatat sint in proident excepteur adipisicing et eiusmod ex qui. Do enim dolore qui laboris tempor in eiusmod. Voluptate sunt irure commodo laboris enim veniam aliqua consequat cillum occaecat commodo aute. Sunt commodo exercitation et qui adipisicing ad sit excepteur labore reprehenderit deserunt minim elit.Occaecat officia irure ex laboris anim laboris aliqua nulla ullamco et officia aliqua irure. Deserunt amet tempor mollit adipisicing reprehenderit culpa mollit fugiat eiusmod minim labore cillum. Sint pariatur ad minim cupidatat sit sunt in est exercitation ipsum id eu. Minim cupidatat minim esse reprehenderit tempor aute in. Aute consectetur commodo aliqua veniam culpa exercitation eu est non tempor dolor. Ea cupidatat duis laborum enim.
+            </Text>
+          </View>
+        );
+      case 'C':
+        return (
+          <View style={styles.additionalInfoContainer}>
+            <Text style={styles.additionalInfoText}>
+            Tempor nulla ad id irure eu sint pariatur non ad aliquip commodo Lorem labore Lorem. Pariatur cillum quis aliquip nostrud cupidatat quis. Laboris sit nostrud ad ullamco. Do anim magna in et velit mollit enim veniam id pariatur Lorem ut. Tempor nulla ex adipisicing amet sit ad aute sunt aliquip do reprehenderit ullamco nisi sint. Labore minim ex exercitation exercitation laborum duis veniam anim do ut fugiat. Deserunt incididunt magna amet exercitation adipisicing consectetur adipisicing cillum laborum amet et veniam esse.
+            </Text>
           </View>
         );
       default:
@@ -40,7 +68,12 @@ const AnnounceScreen = ({ route }) => {
 
   return (
     <View style={styles.container}>
-      {renderContent()}
+      <ScrollView contentContainerStyle={styles.scrollContainer}>
+        <View style={styles.contentWrapper}>
+          {renderContent()}
+        </View>
+        {renderAdditionalInfo()}
+      </ScrollView>
     </View>
   );
 };
@@ -48,27 +81,46 @@ const AnnounceScreen = ({ route }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "white",
+    backgroundColor: 'white',
     padding: 20,
+  },
+  scrollContainer: {
+    flexGrow: 1,
+  },
+  contentWrapper: {
+    alignItems: 'center', // จัดตำแหน่งให้ตรงกลาง
+    marginTop: 85, // ปรับระยะห่างจากด้านบนของหน้าจอ
   },
   contentContainer: {
     width: width * 0.9,
+    paddingTop: 14,
     padding: 20,
-    borderRadius: 10,
+    borderRadius: 15,
     backgroundColor: '#f5f5f5',
+    marginBottom: 15, // เพิ่มระยะห่างระหว่างกล่อง
   },
   header: {
-    fontSize: 22,
-    fontFamily: 'Prompt-Bold',
+    fontSize: 20,
+    fontFamily: 'Prompt-Medium',
     color: '#f65d3c',
-    marginBottom: 10,
+    marginBottom: 5,
   },
   date: {
-    fontSize: 18,
+    fontSize: 15,
     fontFamily: 'Prompt-Regular',
-    color: '#1e1e1e',
+    color: '#6e6e6e',
+  },
+  additionalInfoContainer: {
+    width: width * 0.9,
+    padding: 20,
+    borderRadius: 15,
+    backgroundColor: '#f5f5f5', // สีพื้นหลังของกล่องข้อความยาว
+    marginBottom: 20, // เพิ่มระยะห่างระหว่างกล่อง
+  },
+  additionalInfoText: {
+    fontSize: 16,
+    fontFamily: 'Prompt-Regular',
+    color: '#6e6e6e',
   },
 });
 
